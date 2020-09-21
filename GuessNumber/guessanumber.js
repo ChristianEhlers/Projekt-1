0436c6ef9b78177021506b165626b28383375f84
Kenneth <kennethkskristensen@hotmail.com>
2020-09-21 10:45:42 +0200

    changed code to guessa number game

--- GuessNumber/guessanumber.js
+++ GuessNumber/guessanumber.js
@@ -1,1 +1,15 @@
-//
+//Funktion til at generere et tilfældigt tal mellem 1 og 1000
+let randomNumber = Math.floor(Math.random() * 1000) +1;
+console.log(randomNumber);
+console.log('SLET CONSOLE LOG NÅR SPILLET VIRKER');
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
