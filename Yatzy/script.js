ad35d9f0b4f9a907553fed851581810a450ad56b
Kenneth <kennethkskristensen@hotmail.com>
2020-09-22 12:12:55 +0200

    changed file

--- Yatzy/script.js
+++ Yatzy/script.js
@@ -6,10 +6,7 @@ function kast(){
     document.getElementById("terning").innerHTML = '';
 	
 	for (let i = 0; i < 5; i++){
-<<<<<<< HEAD
-	/*document.getElementById('terning' + '<br>');
-	terning.innerHTML = test();
-    }*/
+	
     var div = document.getElementById("terning");
    var br = document.createElement("br");
    var input = document.createElement("INPUT");
@@ -19,20 +16,6 @@ input.setAttribute("type", "checkbox");
        div.appendChild(input)
        div.appendChild(br);
 }
-=======
-    	var div = document.getElementById("terning");
-   		var br = document.createElement("br");
-   		var input = document.createElement("INPUT");
-		input.setAttribute("type", "checkbox");
-		let udfald = test();
-		var print = document.createTextNode(udfald);
-		if(!locked[i])
-			arr1[udfald]++;
-    	div.appendChild(print);
-    	div.appendChild(input)
-    	div.appendChild(br);
-	}
->>>>>>> 0cca5294b2e8c7787d61bd731a8b0922a42aaa40
 }
 
 function test(){
