f102a8321b2511f2bef8aea04704dd4bbcb0207d
Christian <christiankrushave@gmail.com>
2020-09-22 10:37:53 +0200

    Update script.js

--- Yatzy/script.js
+++ Yatzy/script.js
@@ -6,20 +6,6 @@ function kast(){
     document.getElementById("terning").innerHTML = '';
 	
 	for (let i = 0; i < 5; i++){
-<<<<<<< HEAD
-	/*document.getElementById('terning' + '<br>');
-	terning.innerHTML = test();
-    }*/
-    var div = document.getElementById("terning");
-   var br = document.createElement("br");
-   var input = document.createElement("INPUT");
-input.setAttribute("type", "checkbox");
-   var print = document.createTextNode(test());
-       div.appendChild(print);
-       div.appendChild(input)
-       div.appendChild(br);
-}
-=======
     	var div = document.getElementById("terning");
    		var br = document.createElement("br");
    		var input = document.createElement("INPUT");
@@ -30,14 +16,13 @@ input.setAttribute("type", "checkbox");
 			arr1[udfald]++;
     	div.appendChild(print);
     	div.appendChild(input)
-    	div.appendChild(br);
-	}
->>>>>>> 0cca5294b2e8c7787d61bd731a8b0922a42aaa40
+    	div.appendChild(br);	
 }
+}
+
 
 function test(){
 	dice = Math.random();
     dice = Math.floor(dice * 6 + 1);
     return dice;
-
-	}
+}
