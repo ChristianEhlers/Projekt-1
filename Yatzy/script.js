b6def21409bd1da37b183614a6a63a3f9d8f72e7
Christian <christiankrushave@gmail.com>
2020-09-22 12:11:01 +0200

    Update script.js

--- Yatzy/script.js
+++ Yatzy/script.js
@@ -1,25 +1,39 @@
+//Array to store dice numbers
 let dice = [1, 2, 3, 4, 5, 6];
+
+//arrays to store "eyes" of rolled dices and for checkboxes
 arr1 = [0,0,0,0,0];
 locked = [false,false,false,false,false];
 
+//function to throw dice, clear dices and "rebuild" the dices again for the second and third throw
 function kast(){
+	//when the function is run, create an empty id
     document.getElementById("terning").innerHTML = '';
 	
+	//for loop to run the test function 5 times - each time creating the dices in the "terning" div as well as creating input checkboxes. 
 	for (let i = 0; i < 5; i++){
     	var div = document.getElementById("terning");
    		var br = document.createElement("br");
    		var input = document.createElement("INPUT");
 		input.setAttribute("type", "checkbox");
+		input.addEventListener('click', function(){
+			locked[i] = locked[i] ? false : true;
+		});
 		let udfald = test();
 		var print = document.createTextNode(udfald);
-		if(!locked[i])
-			arr1[udfald]++;
+		
+		if(!locked[i]){
+			arr1[i] = udfald;
+			print = document.createTextNode(udfald);
+		} else{
+			print = document.createTextNode(arr1[i]);
+		}
     	div.appendChild(print);
     	div.appendChild(input)
     	div.appendChild(br);	
+	}
+	console.log(locked);
 }
-}
-
 
 function test(){
 	dice = Math.random();
