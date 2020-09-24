ac2ddcaac85d346244f77ede9abc2272725d9afa
Christian <christiankrushave@gmail.com>
2020-09-24 10:12:07 +0200

    Hangman - sidste rettelser

--- Hangman/hangman.js
+++ Hangman/hangman.js
@@ -1,80 +1,71 @@
-var lsgwoerter = [ "LION", "RHINO", "MONKEY", "HIPPO", "GIRAF", ]
-    var random = Math.floor((Math.random()*(lsgwoerter.length))); 
+var arr1 = [ "LION", "RHINO", "MONKEY", "HIPPO", "GIRAF", ]
+    var random = Math.floor((Math.random()*(arr1.length))); 
     
-    var lsgwort = lsgwoerter[random]; // the word to guess will be chosen from the array above
-    var ratewort = new Array(lsgwort.length);
-    var fehler = 0;
+    var wordChoice = arr1[random]; // the word to guess will be chosen from the array above
+    var chosenWord = new Array(wordChoice.length);
+    var fail = 0;
     
     // every letter in the word is symbolized by an underscore in the guessfield
-    for (var i = 0; i < ratewort.length; i++){
-        ratewort[i] = "_ ";
+    for (var i = 0; i < chosenWord.length; i++){
+        chosenWord[i] = "_ ";
     }
     
     // prints the guessfield
-    function printRatewort(){
-        for (var i = 0; i < ratewort.length; i++){
-        var ratefeld = document.getElementById("ratefeld");
-        var buchstabe = document.createTextNode(ratewort[i]);
-        ratefeld.appendChild(buchstabe);
+    function printchosenWord(){
+        for (var i = 0; i < chosenWord.length; i++){
+        var word = document.getElementById("word");
+        var letter = document.createTextNode(chosenWord[i]);
+        word.appendChild(letter);
         }
     }
     
     //checks if the the letter provided by the user matches one or more of the letters in the word
-    var pruefeZeichen = function(){
-        var f = document.rateformular; 
-        var b = f.elements["ratezeichen"]; 
-        var zeichen = b.value; // the letter provided by the user
-        zeichen = zeichen.toUpperCase();
-        for (var i = 0; i < lsgwort.length; i++){
-            if(lsgwort[i] === zeichen){
-                ratewort[i] = zeichen + " ";
-                var treffer = true;
+    var checkLetter = function(){
+        var f = document.gameInterface; 
+        var b = f.elements["guessField"]; 
+        var letterGuess = b.value; // the letter provided by the user
+        letterGuess = letterGuess.toUpperCase();
+        for (var i = 0; i < wordChoice.length; i++){
+            if(wordChoice[i] === letterGuess){
+                chosenWord[i] = letterGuess + " ";
+                var hit = true;
             }
         b.value = "";
         }
         
         //deletes the guessfield and replaces it with the new one
-        var ratefeld = document.getElementById("ratefeld");
-        ratefeld.innerHTML=""; 
-        printRatewort();
+        var word = document.getElementById("word");
+        word.innerHTML=""; 
+        printchosenWord();
         
         // if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
-        if(!treffer){
-            var gerateneBuchstaben = document.getElementById("gerateneBuchstaben");
-            var buchstabe = document.createTextNode(" " + zeichen);
-            gerateneBuchstaben.appendChild(buchstabe); 
-            fehler++;
+        if(!hit){
+            var wrongLetter = document.getElementById("wrongLetter");
+            var letter = document.createTextNode(" " + letterGuess);
+            wrongLetter.appendChild(letter); 
+            fail++;
             var hangman = document.getElementById("hangman");
-        hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + fehler + ".png";
         }
         
         //checks if all letters have been found
-        var fertig = true;
-        for (var i = 0; i < ratewort.length; i++){
-            if(ratewort[i] === "_ "){
-                fertig = false;
+        var done = true;
+        for (var i = 0; i < chosenWord.length; i++){
+            if(chosenWord[i] === "_ "){
+                done = false;
             }
         }
-        if(fertig){
+        if(done){
             window.alert("You win!");
         }
         
         //once you got six wrong letters, you lose
-        if(fehler === 6){
+        if(fail === 6){
             window.alert("Uh...I guess you're dead now.");
         }
     }
     
     function init(){
-        printRatewort();
+        printchosenWord();
     }
     
     window.onload = init;
-
-    
-   
-    document.addEventListener('click', function (event) {
-        if ( event.target.classList.contains( 'terning' ) ) {
-            console.log("hej")
-        }
-    }, false);
